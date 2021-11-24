ed1f597c0616b77d9162fc541f855363f1ee0872
levan <lingevan0208@gmail.com>
2021-11-24 16:31:41 +0800

    pp cfg classes edit

--- configs/pp/pp_base.py
+++ configs/pp/pp_base.py
@@ -2,22 +2,26 @@ _base_ = '../faster_rcnn/faster_rcnn_r50_caffe_fpn_mstrain_1x_coco.py'
 
 dataset_type = 'CocoDataset'
 data_root = 'datasets/june2021collated/'
+classes=('ship',)
 data = dict(
     train=dict(
         ann_file=data_root + 'train.json',
         img_prefix=data_root + 'images/',
+        classes=classes
         ),
     val=dict(
         ann_file=data_root + 'val.json',
         img_prefix=data_root + 'images/',
+        classes=classes
         ),
     test=dict(
         ann_file=data_root + 'val.json',
         img_prefix=data_root + 'images/',
+        classes=classes
         ))
         
 model = dict(
-    roi_head=dict(bbox_head=dict(num_classes=1)),
+    roi_head=dict(bbox_head=dict(num_classes=len(classes))),
     init_cfg=dict(
         type='Pretrained',
         checkpoint='weights/faster_rcnn_r50_caffe_fpn_mstrain_3x_coco_20210526_095054-1f77628b.pth'
