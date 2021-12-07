309f5db7dbe806f9f56c95441e44b9dd7cd2897e
jin <jinmingteo95@gmail.com>
2021-12-07 14:40:09 +0800

    Trial faster-rcnn and yolox model configuration

---
+++ configs/fish/faster_rcnn.py
@@ -0,0 +1,40 @@
+_base_ = '../faster_rcnn/faster_rcnn_r50_fpn_mstrain_3x_coco.py'
+classes = ('vehicle',)
+
+model = dict(
+    roi_head=dict(bbox_head=dict(num_classes=len(classes))),
+    init_cfg=dict(
+        type='Pretrained',
+        checkpoint='weights/faster_rcnn_r50_caffe_fpn_mstrain_3x_coco_20210526_095054-1f77628b.pth'
+    )
+)
+
+dataset_type = 'CocoDataset'
+data_root = 'datasets/'
+data = dict(
+    train=dict(
+        ann_file=data_root + 'train/train.json',
+        img_prefix=data_root + 'train/images/',
+        classes=classes,
+        ),
+    val=dict(
+        ann_file=data_root + 'val/val.json',
+        img_prefix=data_root + 'val/images/',
+        classes=classes,
+        ),
+    test=dict(
+        ann_file=data_root + 'test/test.json',
+        img_prefix=data_root + 'test/images/',
+        classes=classes,
+        ))
+
+evaluation = dict(interval=1, metric='bbox', save_best='bbox_mAP_50')
+runner = dict(_delete_=True, type='EpochBasedRunner', max_epochs=24)
+checkpoint_config = dict(_delete_=True)
+work_dir = './work_dirs/output'
+
+log_config = dict(
+    hooks=[
+        dict(type='TextLoggerHook'),
+        dict(type='TensorboardLoggerHook')
+    ])
---
+++ configs/fish/yolox_s.py
@@ -0,0 +1,43 @@
+_base_ = '../yolox/yolox_s_8x8_300e_coco.py'
+classes = ('vehicle',)
+
+model = dict(
+    bbox_head=dict(
+        type='YOLOXHead', num_classes=len(classes), in_channels=128, feat_channels=128),
+    init_cfg=dict(
+        type='Pretrained',
+        checkpoint='weights/yolox_s_8x8_300e_coco_20211121_095711-4592a793.pth'
+    )
+)
+
+dataset_type = 'CocoDataset'
+data_root = 'datasets/'
+
+data = dict(
+    train=dict(
+        dataset=dict(
+            ann_file=data_root + 'train/train.json',
+            img_prefix=data_root + 'train/images/',
+            classes=classes,
+        )),
+    val=dict(
+        ann_file=data_root + 'val/val.json',
+        img_prefix=data_root + 'val/images/',
+        classes=classes,
+        ),
+    test=dict(
+        ann_file=data_root + 'test/test.json',
+        img_prefix=data_root + 'test/images/',
+        classes=classes,
+        ))
+
+evaluation = dict(interval=1, metric='bbox', save_best='bbox_mAP_50')
+runner = dict(_delete_=True, type='EpochBasedRunner', max_epochs=24)
+checkpoint_config = dict(_delete_=True)
+work_dir = './work_dirs/output'
+
+log_config = dict(
+    hooks=[
+        dict(type='TextLoggerHook'),
+        dict(type='TensorboardLoggerHook')
+    ])
