2c755a17b17ee886fa5ac9303701ba1dc63319a9
dh <dh@pop-os.localdomain>
2021-12-10 17:22:01 +0800

    Use docker image to init clearml task

--- docker/start_docker.sh
+++ docker/start_docker.sh
@@ -1,5 +1,7 @@
 pardir="$(dirname "$PWD")"
 tag=2.18.0-torch1.10.0
 clearml=~/clearml.conf
+cert_path=/usr/local/share/ca-certificates
 
-docker run -it --gpus all -v $clearml:/root/clearml.conf -v $pardir:$pardir -w $pardir  --shm-size=8g mmdetection:$tag 
+docker run -it --gpus all -v $clearml:/root/clearml.conf -v $REQUESTS_CA_BUNDLE:$REQUESTS_CA_BUNDLE -v $cert_path:$cert_path -v $pardir:$pardir -w $pardir  --shm-size=8g \
+--env AWS_ACCESS_KEY --env AWS_SECRET_ACCESS --env CERT_PATH --env CERT_DL_URL --env REQUESTS_CA_BUNDLE mmdetection:$tag
