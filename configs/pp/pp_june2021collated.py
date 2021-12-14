8171ae47b174607dfaddf47b0b76089d2b906791
levan <lingevan0208@gmail.com>
2021-12-14 17:33:06 +0800

    add new config

---
+++ configs/pp/pp_june2021collated.py
@@ -0,0 +1,18 @@
+_base_ = './pp_modir.py'
+
+s3_data_root = 's3://pixelplus-s3/data/ObjectDetection/june2021collated/'
+local_data_root = 'datasets/june2021collated/'
+
+data = dict(
+    train=dict(
+        ann_file=local_data_root + 'train.json',
+        img_prefix=s3_data_root + 'images/',
+        ),
+    val=dict(
+        ann_file=local_data_root + 'val.json',
+        img_prefix=s3_data_root + 'images/',
+        ),
+    test=dict(
+        ann_file=local_data_root + 'val.json',
+        img_prefix=s3_data_root + 'images/',
+        ))
